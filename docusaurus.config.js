25034ae4d97b1543cd546831368f62633223a6c2
Imke Niewöhner <78965863+NimbleNarniacs@users.noreply.github.com>
2022-10-07 10:14:14 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'debevet Handbuch',
-    url: 'https://debevet.com',
+    url: 'http://handbuch.debevet.de',
     baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
